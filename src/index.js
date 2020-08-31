5b22dc82bab39554a3dddb56d7956cf261a0491d
tommy <biometricmc@gmail.com>
2020-08-31 18:17:01 +1000

    added ready & message

--- src/index.js
+++ src/index.js
@@ -17,4 +17,31 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
+client.once('ready', () => {
+	console.log(`Logged into ${client.user.tag}`);
+	client.user.setActivity('Your GitHub', { type: 'WATCHING' });
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(client.config.prefix) || message.author.bot) return;
+	const args = message.content.slice(client.config.prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	if (!command) return;
+	if (command.guildOnly && message.channel.type !== 'text') return message.reply('I can\'t execute that command inside DMs!');
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+		if (command.usage) reply += `\nThe proper usage would be: \`${client.config.prefix}${command.name} ${command.usage}\``;
+		return message.channel.send(reply);
+	}
+
+	try {
+		command.execute(client, message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
 client.login(client.config.token);
