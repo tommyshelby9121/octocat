c5543169a0a7c61ac8db5a34b11c61f6234b89f6
tommy <biometricmc@gmail.com>
2020-09-01 05:14:09 +1000

    created createRepo command

---
+++ src/commands/createRepo.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { notOwnerEmbed, noRepoNameEmbed, repoCreated } = require('../utils/embed');
+
+module.exports = {
+	name: 'createrepo',
+	description: 'Create a repository',
+	usage: '<repo name> [public/private]',
+	async execute(client, message, args, github) {
+		const owner = process.env.OWNER_ID;
+		if (message.author.id !== owner) {
+			await message.react('❌');
+			notOwnerEmbed(client, message);
+		}
+		const repoName = args[0];
+		if (!repoName) {
+			await message.react('❌');
+			noRepoNameEmbed(client, message);
+		}
+		const repoVisibility = args[1];
+		if (!repoVisibility) {
+			const user = github.getUser();
+			user.createRepo({
+				name: repoName,
+				private: false,
+			}).then((res) => {
+				repoCreated(client, message, res);
+			});
+		}
+	},
+};
