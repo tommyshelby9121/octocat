fc37ef036d0a49514ff9a48abc66f6a0fb9ae811
tommy <biometricmc@gmail.com>
2020-08-31 19:20:56 +1000

    added helpEmbed & version messages

---
+++ src/utils/messages.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+'use strict';
+
+module.exports = {
+	help_embed: {
+		title: 'Get Help',
+		description: 'Having issues with the bot or don\'t know where to start? Take a look at the commands for the bot or if you\'re still stuck, join our support guild!',
+		command_list_field: {
+			name: 'Command List',
+			value: 'https://github.com/tommyshelby9121/octocat/blob/master/docs/commands.md',
+		},
+		support_guild_field: {
+			name: 'Support Guild',
+			value: 'https://discord.gg/WcBzBDA',
+		},
+		request: 'Requested By',
+	},
+	version: 'v1.0.0',
+};
