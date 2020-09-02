6341822a2690f6a6edc29b286c3767512f81c0db
tommy <biometricmc@gmail.com>
2020-09-02 20:03:25 +1000

    Added emoji & added check if user exists

--- src/commands/userInfo.js
+++ src/commands/userInfo.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const {userInfo} = require('../utils/embed')
+const { noUser, userInfo } = require('../utils/embed');
 
 module.exports = {
 	name: 'userinfo',
 	description: 'Get specified users info',
 	usage: '[username]',
-	async execute(client, message, args, github) {
+	execute(client, message, args, github) {
 		const user = github.getUser(args[0]);
-		user.getProfile((err, profile) => {
-			if (err) throw err;
+		user.getProfile(async (err, profile) => {
+			if (err) {
+				await message.react('❌');
+				return noUser(client, message);
+			}
+			await message.react('✅');
 			userInfo(client, message, profile);
 		});
 	},
